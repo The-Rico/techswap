51795d8b46c72226368a7c80cd4e1dd3666ac58a
Rico Rodriguez <97664519+The-Rico@users.noreply.github.com>
2022-06-14 15:55:39 +0900

    header and footer components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import ReactDOM from 'react-dom';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import { Paper } from '@mui/material';
+import { styled } from '@mui/material/styles';
 
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid
+          justifyContent='center'
+          alignItems='center'
+          container
+          rowSpacing={4}
+          columnSpacing={{ xs: 1, sm: 2, md: 7 }}
         >
-          Learn React
-        </a>
-      </header>
+          <Grid item xs={8}>
+            <Item>Hero</Item>
+          </Grid>
+          <Grid item xs={6}>
+            <Item>xs=6</Item>
+          </Grid>
+          <Grid item xs={6}>
+            <Item>xs=6</Item>
+          </Grid>
+          <Grid item xs={12}>
+            <Item>Footer</Item>
+          </Grid>
+        </Grid>
+      </Box>
     </div>
   );
 }
